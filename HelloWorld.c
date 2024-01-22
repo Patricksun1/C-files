3945bdb6744ba8bd4d3bd6284ddeae026a4483be
Patricksun1 <patricksun70@gmail.com>
2024-01-22 10:14:10 +0800

    while

--- HelloWorld.c
+++ HelloWorld.c
@@ -124,22 +124,126 @@ int main(void) {
 */
 
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+
+/*int main(){
     int size;
     printf("Enter size: ");
     scanf("%d",&size);
-    int i = 0;
-    int j = 0;
 
+    int i=0;
+    
     while(i<size){
-    printf("\n(%d,%d)",i,j);    
-    i++;
-        while(j<size){  
+        int j=0;
+        while(j<size){
         printf("(%d,%d)",i,j);    
         j++;
         }
+            printf("\n");
+            i++;
+    }
+    
+
+
+    return 0;
+}
+*/
+
+/*int main(){
+    int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+
+    int row = 0;
+    
+    while(row<size){
+        int column = 0;
+        while(column<size){
+            
+        printf("(%d,%d)",row,column);
+        column++;
+        
+        }
+        printf("\n");
+        row++;
     }
 
+
+    return 0;
+}*/
+
+/*int main(){
+    char letter;
+    int vowels = 0;
+    int consonants = 0;
+    printf("Enter a sequence of characters: ");
+    scanf("%c",&letter);
+
+    while(letter!='\n'){
+    if((letter >= 'a' && letter<= 'z')||(letter >= 'A' && letter <= 'Z')){
+        if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || 
+                letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' || 
+                letter == 'O' || letter == 'U') {
+                vowels++;}
+        else {consonants++;}
+        }
+        scanf("%c",&letter);
+    }
+
+    
+    printf("Number of vowels: %d \n",vowels);
+    printf("Number of consonants: %d\n",consonants);
+
+    return 0;
+}
+*/
+/*struct message {
+    char letter1;
+    char letter2;
+    char letter3;
+    char letter4;
+}
+
+int main(void){
+    char input;
+    int numbers;
+    printf("Message: ");
+    scanf("%c",&input);
+    printf("Enter numbers to encipher by: ");
+    scanf("%d",&numbers);
+
+
+    return 0;
+}
+*/
+
+/*int main(){
+    char name[25];
+    printf("What is your name: ");
+    fgets(name,25,stdin);
+    name[strlen(name)-1] = '\0';
+
+    while(strlen(name)==0){
+    printf("You did not enter your name\n");
+    printf("What is your name: ");
+    fgets(name,25,stdin);
+    name[strlen(name)-1] = '\0';}
+
+    printf("Hello %s\n",name);
+    
+    return 0;
+}*/
+
+int main(){
+    char name[25];
+    printf("What is your name? ");
+    fgets(name,25,stdin);
+    while(strlen(name)-1 == '\0'){
+        printf("You did not enter your name\n");
+        printf("What is your name? ");
+        fgets(name,25,stdin);
+    }
+    printf("Hello %s",name);
     return 0;
 }
